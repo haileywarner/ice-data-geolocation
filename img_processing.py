4e36f1bb45f0f8f0739202db4fbeceb045cfd81c
hwarner <hlwarner@asu.edu>
2023-10-29 21:36:07 -0700

    removed duplicate functions.

--- img_processing.py
+++ img_processing.py
@@ -2,7 +2,6 @@ import xml.etree.ElementTree as et
 import matplotlib.pyplot as plt
 
 def get_img_time(xmlfile):
-
     '''
     get_img_time : Parses military time value from .xml files that accompany IceSat-2 survery campaign
                    imagery, then converts to UTC decimal seconds per day.
@@ -13,12 +12,25 @@ def get_img_time(xmlfile):
     :xml_file: path to .xml file.
     :return: time (hhmmss).
     '''
-
     tree = et.parse(xmlfile)
     root = tree.getroot()
     military = str(root[2][7][0].text).replace(':','')
-    #print(military)
     utc = float(military[0:2])*3600 + float(military[2:4])*60 + float(military[4:6])
-
     return utc
 
+
+def get_img_lon_lat(xmlfile):
+    '''
+    get_img_lon_lat : Parses longitude and latitude of aerial image.
+
+                         Coordinate is roughly the geometric center of the image.
+                         (IceBridge Dataset: IS2OLVIS1BCV (https://nsidc.org/data/is2olvis1bcv/versions/1))
+
+    :xml_file: path to .xml file.
+    :return: lat (float), lon (float).
+    '''
+    tree = et.parse(xmlfile)
+    root = tree.getroot()
+    lat = float(root[2][8][0][0][0][0][1].text)
+    lon = float(root[2][8][0][0][0][0][0].text)
+    return lat, lon
--- lvisf2_processing.py
+++ lvisf2_processing.py
@@ -1,6 +1,8 @@
 import os
 import xml.etree.ElementTree as et
 import matplotlib.pyplot as plt
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 def plot_altimetry_track(txtxmlfile):
     '''
@@ -10,20 +12,22 @@ def plot_altimetry_track(txtxmlfile):
     :return:
     '''
 
+    polygon_points = []
     tree = et.parse(txtxmlfile)
     root = tree.getroot()
     p = root.iter('Point')
-    print(sum(1 for _ in p))
+    #print(sum(1 for _ in p))
     for i in range(0,sum(1 for _ in p)-1):
-        print(root[2][8][0][0][0][i][0].text)
-        print(root[2][8][0][0][0][i][1].text)
-
+        lon = float(root[2][8][0][0][0][i][0].text)
+        lat = float(root[2][8][0][0][0][i][1].text)
+        polygon_points.append((lon,lat))
+    print(polygon_points)
         #matplotlib fill()
 
     return
 
-def get_txtxml_time(xmlfile):
 
+def get_txtxml_time(xmlfile):
     '''
     get_xml_time : Parses military time value from .xml files that accompany IceSat-2 survery campaign
                    imagery, then converts to UTC decimal seconds per day.
@@ -34,29 +38,25 @@ def get_txtxml_time(xmlfile):
     :xml_file: path to .xml file.
     :return: time (hhmmss).
     '''
-
     tree = et.parse(xmlfile)
     root = tree.getroot()
     start_military = str(root[2][7][2].text).replace(':','')
     end_military   = str(root[2][7][0].text).replace(':','')
-    #print(start_military)
-    #print(end_military)
     t0 = float(start_military[0:2])*3600 + float(start_military[2:4])*60 + float(start_military[4:6])
     tn = float(end_military[0:2])*3600 + float(end_military[2:4])*60 + float(end_military[4:6])
-
     return t0, tn
 
-def get_txt_time(txtfile):
+
+def get_txt_time_range(txtfile):
 
     '''
-    get_txt_time : Parses LVISF2 TXT files for start and end time of segment in UTC decimal seconds per day.
+    get_txt_time_range : Parses LVISF2 TXT files for start and end time of segment in UTC decimal seconds per day.
 
-                   (IceBridge Dataset: IS2OLVIS1BCV (https://nsidc.org/data/lvisf2/versions/1))
+                         (IceBridge Dataset: LVISF2 (https://nsidc.org/data/lvisf2/versions/1))
 
     :xml_file: path to .TXT file.
-    :return:   (t0, tn)
+    :return:   t0 (s), tn (s)
     '''
-
     with open(txtfile, 'rb') as f:
         for i, line in enumerate(f):
             if i == 14:
@@ -68,23 +68,4 @@ def get_txt_time(txtfile):
         except OSError:
             f.seek(0)
         tn = float(f.readline().decode()[22:33])
-
     return t0, tn
-
-def get_txtxml_lon_lat(xmlfile):
-    '''
-    get_xml_time : Parses military time value from .xml files that accompany IceSat-2 survery campaign
-                   imagery, then converts to UTC decimal seconds per day.
-
-                   Coordinate is roughly the geometric center of the image.
-                   (IceBridge Dataset: IS2OLVIS1BCV (https://nsidc.org/data/is2olvis1bcv/versions/1))
-
-    :xml_file: path to .xml file.
-    :return: time (hhmmss).
-    '''
-
-    tree = et.parse(xmlfile)
-    root = tree.getroot()
-    lat = float(root[2][8][0][0][0][0][1].text)
-    lon = float(root[2][8][0][0][0][0][0].text)
-    return lat, lon
