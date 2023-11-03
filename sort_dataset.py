fd2e967a9bb83735c220b10e7185f3e958e059ba
hwarner <hlwarner@asu.edu>
2023-11-02 19:14:37 -0700

    Fixed bugs, added file saving mechanism.

--- sort_dataset.py
+++ sort_dataset.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from lvisf2_processing import check_altimetry_track
+from lvisf2_processing import get_txtxml_time
 from img_processing import get_img_lon_lat
 
 #################################################################################################################################
@@ -17,7 +18,16 @@ for txtxmlfile in glob.glob(r'D:\lvis images + altimetry\altimetry\*.TXT.xml'):
     index = txtxmlfile[-14:-8]
     day   = txtxmlfile[-23:-21]
     h5file = '*.'+index+'.h5'
+    h5xmlfile = h5file+'.xml'
     folder = ''
+
+    t0, tn = get_txtxml_time(txtxmlfile)
+    new_folder = ('E:\spatially_sorted_lvisf2_is2olvis1bcv\\'+str(t0)+'_to_'+str(tn)+'\\')
+    if os.path.exists(new_folder) == True:
+        continue
+    else:
+        os.mkdir(new_folder)
+
     if   day == '11':
         folder = HOME_DIR+r'\2022.07.11'
     elif day == '12':
@@ -30,14 +40,19 @@ for txtxmlfile in glob.glob(r'D:\lvis images + altimetry\altimetry\*.TXT.xml'):
         folder = HOME_DIR+r'\2022.07.23'
     elif day == '26':
         folder = HOME_DIR+r'\2022.07.26'
-    else: print('Error: invalid TXT file name.')
+    else:
+        print('Error: invalid TXT file name.')
+        break
 
     row = 0
-    #img_names = []
+    img_names = []
+    imgxml_names = []
     img_points = np.zeros((2006,2), dtype=float)
     for imgxmlfile in glob.glob(folder+'\*CAM150MP*.xml'):
-        #imgfile = imgxmlfile[:-4]
-        #img_names.append(imgfile)
+        imgfile = imgxmlfile[:-4]
+        img_names.append(imgfile)
+        imgxml_names.append(imgxmlfile)
+
         tree = et.parse(imgxmlfile)
         root = tree.getroot()
         lat = float(root[2][8][0][0][1].text)
@@ -46,9 +61,49 @@ for txtxmlfile in glob.glob(r'D:\lvis images + altimetry\altimetry\*.TXT.xml'):
         img_points[row][1] = lat
         #print(lon, lat)
         row += 1
+
     #print(img_points)
     boolarr = check_altimetry_track(txtxmlfile, img_points)
     print('BOOLARR: '+str(boolarr.sum()))
+
+    indices = boolarr.nonzero()
+    print(indices)
+    if indices[0].size != 0:
+        for i in range(len(indices[0])):
+            save = img_names[int(indices[0][i])]
+            savexml = save[:-3]+'TIF.xml'
+
+            # Save IMG XML file to new drive
+            if os.path.isfile(new_folder+os.path.basename(savexml)) == False:
+                #shutil.copy(savexml, new_folder+os.path.basename(savexml))
+                print(new_folder+os.path.basename(savexml))
+            # Save IMG TIF file to new drive
+            if os.path.isfile(new_folder+os.path.basename(save)) == False:
+                #shutil.copy(save, new_folder+os.path.basename(save))
+                print(new_folder+os.path.basename(save))
+    
+    # Save LVISF2 TXT XML file to new drive
+    if os.path.isfile(new_folder+os.path.basename(txtxmlfile)) == False:
+         #shutil.copy(txtxmlfile, new_folder+os.path.basename(txtxmlfile))
+        print(new_folder+os.path.basename(txtxmlfile))
+
+    # Save LVISF2 TXT file to new drive
+    if os.path.isfile(new_folder+os.path.basename(txtfile)) == False:
+        #shutil.copy(txtfile, new_folder+os.path.basename(txtfile))
+        print(new_folder+os.path.basename(txtfile))
+
+    # Save LVISL1B h5 XML file to new drive
+    if os.path.isfile(new_folder+os.path.basename(h5xmlfile)) == False:
+        #shutil.copy(h5xmlfile, new_folder+os.path.basename(h5xmlfile))
+        print(new_folder+os.path.basename(h5xmlfile))
+
+    # Save LVISL1B h5 file to new drive
+    if os.path.isfile(new_folder+os.path.basename(h5file)) == False:
+         #shutil.copy(h5file, new_folder+os.path.basename(h5file))
+        print(new_folder+os.path.basename(h5file))
+
+    #COPY OVER INBOUND IMG&XML FILES, WAVEFORM, AND ALTIMETRY TO NEW DRIVE
+
     #print(boolarr.nonzero())
     # for i in range(len(img_index)):
     #     print(img_names[img_index[i]])
